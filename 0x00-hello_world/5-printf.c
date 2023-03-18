f624495a39a92f31c8dfd143b3068b649920c7ca
moulineE <elmahdi.mouline@live.fr>
2023-03-18 16:10:49 +0000

    task 5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *write a strig to stdout
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
