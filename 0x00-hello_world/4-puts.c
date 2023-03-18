4ef59bdae51ba7849951b4eaaffe27a6488f63dc
moulineE <elmahdi.mouline@live.fr>
2023-03-18 15:42:34 +0000

    task 4. Hello, puts betty error fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ *write a strig to stdout
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return(0);
+	return (0);
 }
