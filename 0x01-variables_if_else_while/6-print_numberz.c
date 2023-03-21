a23a6003372db85e916a8075ede7fd470f8349f9
moulineE <elmahdi.mouline@live.fr>
2023-03-21 06:33:44 +0000

    task 6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Enrty point
+ *
+ * declare int n
+ *
+ * while structure with putchar
+ * prints all single digit numbers of base 10 starting from 0
+ *
+ * print a new line
+ *
+ * Return: EXIT_SUCCESS
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+
+	while(n < 10)
+	{
+		putchar('0' + n);
+		n = n + 1;
+	}
+	putchar('\n');
+
+	return (EXIT_SUCCESS);
+}
