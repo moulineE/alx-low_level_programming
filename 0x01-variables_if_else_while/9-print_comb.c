2779ef8004c4c74e3ec94f5652e1a250cb9dd3b2
moulineE <elmahdi.mouline@live.fr>
2023-03-21 07:44:31 +0000

    task 9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Enrty point
+ *
+ * declare int n
+ *
+ * while structure with putchar and nested if to add 'space' and ','
+ * prints all single digit numbers of base 10 starting from 0
+ *
+ * print a new line
+ *
+ * Return: EXIT_SUCCESS
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+
+	while (n < 10)
+	{
+		putchar('0' + n);
+		n = n + 1;
+		if (n < 10)
+		{
+		putchar(44);
+		putchar(32);
+		}
+	}
+	putchar('\n');
+
+	return (EXIT_SUCCESS);
+}
