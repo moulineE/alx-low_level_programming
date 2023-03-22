fd96749e01702019674d3a0c524d281bfd59862e
moulineE <elmahdi.mouline@live.fr>
2023-03-22 02:40:41 +0000

    task 0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * print "_putchar" with printf followed by a new line.
+ *
+ * Return: Always 0 (SUCCESS)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
