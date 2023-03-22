9513120301c461efd5dca75183cdc227f6a2d40e
moulineE <elmahdi.mouline@live.fr>
2023-03-22 21:16:06 +0000

    task 2. 10 x alphabet betty fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,24 @@
 #include <unistd.h>
 #include "main.h"
 /**
- *  print_alphabet_10x - prints the alphabet 10x, in lowercase, followed by a new line
+ *  print_alphabet_x10 - prints the alphabet 10x, in lowercase,
+ *  followed by a new line
  *
  *  Return: void
  */
 void print_alphabet_x10(void)
 {
 	int a;
+
 	for (a = 0; a < 10; ++a)
 	{
 		int i = 97;
+
 		while (i <= 122)
 		{
 			_putchar(i);
 			i = i + 1;
 		}
 		_putchar('\n');
-	}	
+	}
 }
