ff4b037d2caa4fdda9b94cd2473edfa283632d04
moulineE <elmahdi.mouline@live.fr>
2023-03-24 02:01:21 +0000

    task 10. a + b betty fix

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,4 +1,11 @@
 #include "main.h"
+/**
+ * add - adds two integers and returns the result
+ * @a: 1st int to add
+ * @b: 2nd int to add
+ *
+ * Return: the somme 'm'
+ */
 int add(int a, int b)
 {
 	int m;
