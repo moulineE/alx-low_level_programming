f1d6b11aef023e2549a725f2b98de29872fa4fdd
moulineE <elmahdi.mouline@live.fr>
2023-04-01 02:42:31 +0000

    task 3. strcmp betty fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,12 @@
 #include "main.h"
+/**
+ * _strcmp -  function that compares two strings
+ * function should work exactly like strcmp
+ * @s1: 1st string to compar
+ * @s2: 2nd string to compar
+ *
+ * Return: s1 - s2
+ */
 int _strcmp(char *s1, char *s2)
 {
 	while (*s1 != '\0' && (*s1 == *s2))
@@ -6,5 +14,5 @@ int _strcmp(char *s1, char *s2)
 		s1++;
 		s2++;
 	}
-	return *(unsigned char*)s1 - *(unsigned char*)s2;
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
