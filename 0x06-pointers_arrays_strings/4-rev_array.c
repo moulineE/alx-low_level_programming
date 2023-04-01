4028b964f2c3ad4e89d5ff034d986d2b3c8f9e9b
moulineE <elmahdi.mouline@live.fr>
2023-04-01 03:31:29 +0000

    task 4. I am a kind of paranoid in reverse. I suspect people of plotting to make me happy hotfix betty

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+ * reverse_array - function that reverses the content of an array of integers
+ * @a: the int array to inverse
+ * @n: the number of elements of the array
+ */
 void reverse_array(int *a, int n)
 {
 	int i;
