92e8eca7ee9ee660d232cb407f2c97d991320372
moulineE <elmahdi.mouline@live.fr>
2023-04-03 10:19:56 +0000

    task 6. Expect the best. Prepare for the worst. Capitalize on what comes hotfix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- * *cap_string - Write a function that
- * changes all lowercase letters to uppercase.
+ * *cap_string - function that capitalizes all words of a string.
  * @s: the string to change
  *
  * Return: the string in uppercase
@@ -9,12 +8,27 @@
 char *cap_string(char *s)
 {
 	int i;
+	int m;
+	int b[13] = {32, 9, 10, 44, 59, 46, 33, 63, 147, 40, 41, 123, 125};
 
 	for (i = 0; s[i] > 0; i++)
 	{
 		if (s[i] > 96 && s[i] < 123)
 		{
-			s[i] = (s[i] - 32);
+			if (i == 0)
+			{
+				s[i] = (s[i] - 32);
+			}
+			else if (s[i] != 32)
+			{
+				for (m = 0; m < 13; m++)
+				{
+					if (s[i - 1] == b[m])
+					{
+						s[i] = (s[i] - 32);
+					}
+				}
+			}
 		}
 	}
 
