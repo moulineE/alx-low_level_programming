343d482041833caefeea9b352222a120342044d7
moulineE <elmahdi.mouline@live.fr>
2023-04-04 02:53:24 +0000

    task 4. strpbrk betty fix

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,4 +1,11 @@
 #include "main.h"
+/**
+ * *_strpbrk - function that searches a string for any of a set of bytes
+ * @s: the searched string
+ * @accept: the liste of byte to locate
+ *
+ * Return: a pointer to the first occurence of accept, or null if not found
+ */
 char *_strpbrk(char *s, char *accept)
 {
 	int i;
