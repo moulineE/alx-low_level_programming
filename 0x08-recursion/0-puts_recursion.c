73165a2f2c0bf7bfdef27e5c4d7772500644538c
moulineE <elmahdi.mouline@live.fr>
2023-04-06 02:16:19 +0000

    task 0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget hotfix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,13 +5,13 @@
  */
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	s++;
 	if (*s > 0)
 	{
+		_putchar(*s);
+		 s++;
 		_puts_recursion(s);
 	}
-	if (*s == 0)
+	else
 	{
 		_putchar('\n');
 	}
