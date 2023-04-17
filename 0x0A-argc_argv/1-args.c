b2443bb0b2d45ddb97dfcd684cae88e7b8b85263
moulineE <elmahdi.mouline@live.fr>
2023-04-17 01:29:01 +0000

    task 1. Silence is argument carried out by other means betty fix

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+/**
+ * main - program that prints the number of arguments passed into it.
+ * @argc: the number of arguments
+ * @argv: the array of arguments
+ *
+ * Return: 0
+ */
 int main(int argc, __attribute__((unused)) char *argv[])
 {
 	printf("%d\n", (argc - 1));
