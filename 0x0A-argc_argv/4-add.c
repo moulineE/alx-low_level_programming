8a09bfa974ba867a161525c615b4ec4c88bda342
moulineE <elmahdi.mouline@live.fr>
2023-04-17 06:18:18 +0000

    task 4. To infinity and beyond hotfix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int _atoi(char *s);
-int _pow_recursion(int x, int y);
+int _strlen(char *s);
+int _isdigit(int c);
+int digiteste(char *s);
 
 /**
  * main - program that adds positive numbers
@@ -17,82 +18,76 @@ int main(int argc, char *argv[])
 	int i;
 	int m = 0;
 
+	if (argc < 3)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
 	for (i = 1; i < argc; i++)
 	{
-		if (!(*argv[i] >= 48 && *argv[i] <= 57))
+		if (digiteste(argv[i]))
+		{
+			m = m + atoi(argv[i]);
+		}
+		else
 		{
 			printf("Error\n");
 			return (1);
 		}
-		m = m + _atoi(argv[i]);
 	}
 	printf("%d\n", m);
 	return (0);
 }
 /**
- * _atoi - function that convert a string to an integer.
- * @s: the char to convert
+ * digiteste - teste if s is composed of digits
+ * @s:  the char array tested
  *
- * Return: r
+ * Return: 1 if digfit, 0 if not
  */
-int _atoi(char *s)
+int digiteste(char *s)
 {
-	int negf = 0;
 	int i;
-	int n = 0;
-	int r = 0;
-	int df = 0;
 
-	for (i = 0; (s[i] > 0 && ((s[i] >= 48 && s[i] <= 57) || df == 0)); i++)
+	for (i = 0; i < _strlen(s); i++)
 	{
-		if (s[i] >= 48 && s[i] <= 57)
+		if (!(_isdigit(s[i])))
 		{
-			n++;
-			df++;
+			return (0);
 		}
 	}
-	for (i = 0; s[i] < 48; i++)
-	{
-		if (s[i] == 45)
-		{
-			negf++;
-		}
-	}
-	for (i = 0; (s[i] > 0); i++)
-	{
-		if (s[i] >= 48 && s[i] <= 57)
-		{
-			r = r - ((s[i] - 48) * _pow_recursion(10, n - 1));
-			n--;
-		}
-	}
-	if (negf % 2 == 0)
-	{
-		r = r * -1;
-	}
-	return (r);
+	return (1);
 }
 /**
- * _pow_recursion - function that returns the value of
- * x raised to the power of y
- * @x: the value to raise
- * @y: the power factor
- *
- * Return: x^y or if y < 0, -1
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
  */
-int _pow_recursion(int x, int y)
+int _strlen(char *s)
 {
-	if (y < 0)
+	int longi = 0;
+
+	while (*s != '\0')
 	{
-		return (0);
+		longi++;
+		s++;
 	}
-	if (y == 0)
+
+	return (longi);
+}
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: the character to check
+ *
+ * Return: 1 if digit, 0 if not
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
 	{
 		return (1);
 	}
-	if (y > 0)
+	else
 	{
-		return (x * _pow_recursion(x, y - 1));
+		return (0);
 	}
-	return (0);
 }
