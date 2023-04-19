e3725bc5081f8fbf476305267c4432a0c737c8ec
moulineE <elmahdi.mouline@live.fr>
2023-04-19 03:25:43 +0000

    task 0. Poppy betty hotfix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef dog_h
 #define dog_h
-
+/**
+ * struct dog - dog tag info
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
 typedef struct dog
 {
 	char *name;
