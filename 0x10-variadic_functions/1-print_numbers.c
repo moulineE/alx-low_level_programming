10b7fa9151b2c4bdd532071d20c622c13ee3ab08
moulineE <elmahdi.mouline@live.fr>
2023-04-21 01:39:27 +0000

    task 1. If you spend too much time thinking about a thing, you'll never get it done

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - unction that prints numbers, followed by a new line.
+ * @separator: the separator
+ * @n: the number of parameters
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, unsigned int));
+		if (i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+}
