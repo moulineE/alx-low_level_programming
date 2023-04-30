b13c789144483d57e4447fa4b80189dbb66d4237
moulineE <elmahdi.mouline@live.fr>
2023-04-30 03:24:34 +0000

    task 2. Add node taskcheckfix2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -20,7 +20,7 @@ list_t *add_node(list_t **head, const char *str)
 	}
 	new->str = strdup(str);
 	new->len = len;
-	new->next = *head;
-	*head = new;
+	new->next = (*head);
+	(*head) = new;
 	return (*head);
 }
