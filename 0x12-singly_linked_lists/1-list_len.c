0341d6a30ae38322df88a56af12663566e475170
moulineE <elmahdi.mouline@live.fr>
2023-04-30 02:00:57 +0000

    task 1. List length BETTY FIX

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * function that returns the number of elements in a linked list_t list.
+ * list_len - function that returns the number of elements
+ * in a linked list_t list.
  * @h: pointer to the head of the list
  *
  * Return: i, the number of node
