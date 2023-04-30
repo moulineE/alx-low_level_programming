e95bb9d58b14202c31f877b531883d4d430763e6
moulineE <elmahdi.mouline@live.fr>
2023-04-30 16:49:58 +0000

    task 5. The Hare and the Tortoise BETTY FIX

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
-void before_main() __attribute__((constructor));
+void before_main(void) __attribute__((constructor));
+
+/**
+ * before_main - prints before the main function is executed.
+ */
 
 void before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	char *s = "You're beat! and yet, you must allow,";
+	char *s1 = "I bore my house upon my back!";
+
+	printf("%s\n%s\n", s, s1);
 }
