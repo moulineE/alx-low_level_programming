dce2498ca4e3f15ea531c461b815dd6eeef7b765
moulineE <elmahdi.mouline@live.fr>
2023-04-30 01:58:51 +0000

    task 1. List length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * function that returns the number of elements in a linked list_t list.
+ * @h: pointer to the head of the list
+ *
+ * Return: i, the number of node
+ */
+
+size_t list_len(const list_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
