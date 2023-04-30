e0d68eafc1fb36054b0cf1f68f9394acfb0fb2c8
moulineE <elmahdi.mouline@live.fr>
2023-04-30 02:57:06 +0000

    task 2. Add node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+int _strlen(char *s);
+
+/**
+ * *add_node - function that adds a new node at the beginning of a list_t list
+ * @head: pointer to the adress of the firs node
+ * @str: the data of the new node
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL || str == NULL)
+	{
+		printf("error\n");
+		return (NULL);
+	}
+	new->str = strdup(str);
+	new->len = _strlen(strdup(str));
+	new->next = *head;
+	*head = new;
+	return (*head);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+
+	return (longi);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,8 @@
 #ifndef lists_h
 #define lists_h
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
@@ -20,5 +22,6 @@ typedef struct list_s
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 #endif
