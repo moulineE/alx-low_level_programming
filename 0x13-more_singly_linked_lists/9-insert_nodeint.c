db24a58d52c1d72eddc2867973f4f7d922397df1
moulineE <elmahdi.mouline@live.fr>
2023-05-03 02:35:38 +0000

    task 9. Insert FIX

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *prev;
 	unsigned int i = 0;
 
-	if (*head == NULL)
+	if (head == NULL)
 	{
 		return (NULL);
 	}
@@ -29,6 +29,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new = _add_nodeint(&temp, n);
+		*head = new;
+		return (new);
 	}
 	while (idx > i && temp->next != NULL)
 	{
@@ -99,8 +101,7 @@ listint_t *_add_nodeint(listint_t **head, const int n)
 	}
 	new->n = n;
 	new->next = *head;
-	*head = new;
-	return (*head);
+	return (new);
 }
 
 #include "lists.h"
