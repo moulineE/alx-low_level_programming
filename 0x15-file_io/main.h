df2218c3f44a71bd1246228c447482a84c588b26
moulineE <elmahdi.mouline@live.fr>
2023-05-09 03:00:13 +0000

    task 0 fix

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
