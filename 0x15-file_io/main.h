785b1d33306abf77b597149c860cbcd26be3bdff
moulineE <elmahdi.mouline@live.fr>
2023-05-09 03:03:56 +0000

    task 0 fix stdlib.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,10 +1,9 @@
 #ifndef main_h
 #define main_h
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
