35aaaa3bde9096161d0f8761171e96e9a52b4c43
moulineE <elmahdi.mouline@live.fr>
2023-06-16 22:08:30 +0000

    Task 7. Insert at index FIX edgecase: when h == null abd idx > 0

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,6 +16,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i;
 	unsigned int l;
 
+	if ((current == NULL) && (idx > 0))
+		return (NULL);
 	if (idx == 0)
 	{
 		return (add_dnodeint(h, n));
