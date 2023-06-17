9fba24b0d007cfb993a2767a55a46fca83fe5a2f
moulineE <elmahdi.mouline@live.fr>
2023-06-17 01:40:39 +0000

    Task 8. Delete at index FIX return value

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -18,7 +18,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int l = 0;
 
 	if (*head == NULL)
-		return (-1);
+		return (1);
 	if (current == NULL)
 		return (-1);
 	if (current->next == NULL)
