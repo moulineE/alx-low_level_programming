9c004b4ca6358b4e70024e170e8b84cb7c318db2
moulineE <elmahdi.mouline@live.fr>
2023-06-17 01:47:07 +0000

    Task 8. Delete at index FIX edgecase: 1 node in the list and index > list

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -21,7 +21,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (-1);
 	if (current == NULL)
 		return (-1);
-	if (current->next == NULL)
+	if ((current->next == NULL) && (index == 0))
 	{
 		free(*head);
 		*head = NULL;
