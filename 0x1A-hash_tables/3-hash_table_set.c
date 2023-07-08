c8c851d6d32afbc0b16e0d8ebd05bdf3d829ae3b
moulineE <elmahdi.mouline@live.fr>
2023-07-08 04:29:44 +0000

    Task 3. >>> ht['betty'] = 'cool'

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,79 @@
+#include "hash_tables.h"
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+hash_node_t *chaine_node(const char *key, const char *value);
+
+/**
+ * hash_table_set - function that adds an element to the hash table
+ * @ht: a pointer to the hash table
+ * @key: is the key
+ * @value: is the value
+ *
+ * Return:1 if it succeeded, 0 otherwise
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *current;
+
+	if (key == NULL || *key == '\0' || ht == NULL || value == NULL)
+	{
+		return (0);
+	}
+	index = key_index((const unsigned char *) key, ht->size);
+	current = (ht->array[index]);
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = chaine_node(key, value);
+		if (ht->array[index] == NULL)
+			return (0);
+		return (1);
+	}
+	else
+	{
+		(ht->array[index]) = current;
+		while (current != NULL)
+		{
+			if (strcmp(current->key, key) == 0)
+			{
+				free(current->value);
+				current->value = strdup(value);
+				return (1);
+			}
+			if (current->next == NULL)
+			{
+				current->next = chaine_node(key, value);
+				if (current->next == NULL)
+					return (0);
+				return (1);
+			}
+			current = current->next;
+		}
+	}
+	return (0);
+}
+
+/**
+ * chaine_node - creat the data node
+ * @key: the key
+ * @value: the value
+ *
+ * Return: a pointer to the new node
+ */
+
+hash_node_t *chaine_node(const char *key, const char *value)
+{
+	hash_node_t *new;
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->key = strdup(key);
+	new->value = strdup(value);
+	new->next = NULL;
+	return (new);
+}
