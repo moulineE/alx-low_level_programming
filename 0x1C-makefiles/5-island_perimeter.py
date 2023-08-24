23a4807cabef1765da2be4a7b08c72887cbbc6db
moulineE <elmahdi.mouline@live.fr>
2023-08-24 15:32:16 +0100

    Task 5. Island Perimeter FIX edgecase

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,8 +7,8 @@ the perimeter of the island described in grid
 
 def island_perimeter(grid):
     """ returns the perimeter of the island described in grid"""
-    height = len(grid) - 1
-    width = len(grid[0]) - 1
+    height = len(grid)
+    width = len(grid[0])
     perimeter = 0
     for i in range(height):
         for j in range(width):
@@ -17,8 +17,8 @@ def island_perimeter(grid):
                     perimeter += 1
                 if j == 0 or grid[i][j - 1] == 0:
                     perimeter += 1
-                if i == height or grid[i + 1][j] == 0:
+                if i == height - 1 or grid[i + 1][j] == 0:
                     perimeter += 1
-                if j == width or grid[i][j + 1] == 0:
+                if j == width - 1 or grid[i][j + 1] == 0:
                     perimeter += 1
     return perimeter
